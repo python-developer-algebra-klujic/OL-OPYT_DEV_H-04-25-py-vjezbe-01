6eff1576cbc6b542f4635adc7347e1813e4bef18
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 20:53:38 +0200

    Update vjezba_01.py

--- vjezba_01.py
+++ vjezba_01.py
@@ -8,3 +8,17 @@ SAVJET: Za sada koristite zasebnu varijablu za svaki od četiri broja,
         kako je navedeno u primjeru (192.168.0.184).
 Ispis treba napraviti za sve oblike brojevnih sustava.
 '''
+
+a = 192
+b = 168
+c = 0
+d = 184
+
+binary = f'{bin(a)}.{bin(b)}.{bin(c)}.{bin(d)}'
+octal = f'{oct(a)}.{oct(b)}.{oct(c)}.{oct(d)}'
+hexa = f'{hex(a)}.{hex(b)}.{hex(c)}.{hex(d)}'
+
+print()
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u binarnom zapisu je {binary}')
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u oktalnom zapisu je {octal}')
+print(f'Prikaz IP adrese {a}.{b}.{c}.{d} u heksadekadskom zapisu je {hexa}')
