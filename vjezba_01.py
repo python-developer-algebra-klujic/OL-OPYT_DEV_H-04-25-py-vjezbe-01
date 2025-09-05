fc0cb82266f20c075b6df1912a144187a930a94e
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 20:38:13 +0200

    Create vjezba_01.py

---
+++ vjezba_01.py
@@ -0,0 +1,10 @@
+'''
+IP adresa je adresa svakog računala na mreži koja se sastoji od četiri broja između 0 i 256.
+Primjer IP adrese: 192.168.0.184
+
+IP adresu iz primjera ispišite u binarnom, oktalno i heksadekadskom obliku.
+SAVJET: Za sada koristite zasebnu varijablu za svaki od četiri broja, 
+        odnosno dijela (okteta) IP adrese, ali ispišite ih u istom obliku 
+        kako je navedeno u primjeru (192.168.0.184).
+Ispis treba napraviti za sve oblike brojevnih sustava.
+'''
